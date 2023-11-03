3a8c5b08ff3d339c955b3e803c74ffb9952c77d4
Hossein <hosseinpirasbaghi@gmail.com>
2023-11-03 11:26:12 +0330

    db json

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 console.log('hello world')
+let info = { name: 'hossein', age: '22', job: "student" }
+fetch('https://server-contacts.onrender.com/posts', {
+    method: "POST",
+    body: JSON.stringify(info)
+})
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err.message))
 fetch('https://server-contacts.onrender.com/posts')
-.then(res => console.log(res))
+    .then(res => res.json()).then(data => console.log(data))
+    .catch(err => console.log(err.message))
