685a90c2d24a6065627b5f6dacc1f5f2b8949ad1
Hossein <hosseinpirasbaghi@gmail.com>
2023-11-04 18:01:10 +0330

    contacts

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
-console.log('hello world')
+// const { default: axios } = require("axios")
+
+// const { default: axios } = require("axios")
+
+const getData = async () => {
+    try {
+        const {data}=await axios.get('https://server-contacts.onrender.com/comments')
+        console.log(data)
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+getData()
 let info = { name: 'hossein', age: '22', job: "student" }
-fetch('https://server-contacts.onrender.com/posts', {
-    method: "POST",
-    body: JSON.stringify(info)
-})
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err.message))
-fetch('https://server-contacts.onrender.com/posts')
-    .then(res => res.json()).then(data => console.log(data))
-    .catch(err => console.log(err.message))
+const createData=async()=>{
+    try{
+        const data=await axios.post('https://server-contacts.onrender.com/posts',info)
+        console.log(data)
+    }
+    catch(err){
+        console.log(err.message)
+    }
+}
+createData()
+getData()
